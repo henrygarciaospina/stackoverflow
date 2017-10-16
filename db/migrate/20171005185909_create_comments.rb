41e79b7b91eb1145f20bcb73c92ebf1f7868c40d
henrygarciaospina <henrygarcia0309@gmail.com>
2017-10-16 00:16:50 -0500

    Eliminación de relación de clave foranea en los modelos plimorficos ya que genera error al momento de correr las migraciones

--- db/migrate/20171005185909_create_comments.rb
+++ db/migrate/20171005185909_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration[5.1]
   def change
     create_table :comments do |t|
-      t.references :commentable, polymorphic:true, foreign_key: true
+      t.references :commentable, polymorphic:true
       t.string :commentable_type
       t.text :body
 
--- db/schema.rb
+++ db/schema.rb
@@ -23,12 +23,12 @@ ActiveRecord::Schema.define(version: 20171010022702) do
   end
 
   create_table "comments", force: :cascade do |t|
-    t.text "body"
-    t.integer "commentable_id"
     t.string "commentable_type"
+    t.integer "commentable_id"
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["commentable_id"], name: "index_comments_on_commentable_id"
+    t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable_type_and_commentable_id"
   end
 
   create_table "questions", force: :cascade do |t|
@@ -59,13 +59,13 @@ ActiveRecord::Schema.define(version: 20171010022702) do
   end
 
   create_table "votes", force: :cascade do |t|
-    t.integer "voteable_id"
     t.string "voteable_type"
+    t.integer "voteable_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.index ["user_id"], name: "index_votes_on_user_id"
-    t.index ["voteable_id"], name: "index_votes_on_voteable_id"
+    t.index ["voteable_type", "voteable_id"], name: "index_votes_on_voteable_type_and_voteable_id"
   end
 
 end
