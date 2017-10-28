e38b7b3229fb80d4d5296339d3f675f80504937c
henrygarciaospina <henrygarcia0309@gmail.com>
2017-10-28 17:59:47 -0500

    Vista show de questions modificada para alinear contenido de preguntas, respuestas y sus correspondientes comentarios cuando el usuario se encuentra logueado

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,12 +4,12 @@ class VotesController < ApplicationController
 
   def create
     @voteable.votes.create(user: current_user)
-    redirect_to question_path(question), notice: "A successfully voted for the question..."
+    redirect_to question_path(question), notice: "A successfully voted..."
   end
 
   def destroy
     @voteable.votes.where(user: current_user).take.try(:destroy)
-    redirect_to question_path(question), notice: "Successfully removed vote"
+    redirect_to question_path(question), notice: "Successfully removed vote..."
   end
 
   private
